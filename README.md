8b0f9af3570b0eaf4e59abcbef3624803f924dad
Briano Goestiawan <b@briano.io>
2020-11-26 17:27:01 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Gomail
+
+**Requirements**
+
+- Go
+
+**Installation**
+
+```bash
+go get github.com/bbriano/gomail
+```
+
+currently only works with google's mail server
+
+setup environment variables
+
+```bash
+export GOMAIL_USER='yourname@example.com'
+export GOMAIL_PASS='Password123'
+```
+
+**Usage**
+
+Mail someone. Will use default editor
+
+```bash
+gomail someone@example.com
+```
+
+Mail to self
+
+```bash
+gomail
+```
+
+Will read from STDIN if its not empty
+
+```bash
+echo 'hello, world' | gomail someone@example.com
+```
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/smtp"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	from := os.Getenv("GOMAIL_USER")
+	password := os.Getenv("GOMAIL_PASS")
+	to := os.Args[1:]
+	host := "smtp.gmail.com"
+	addr := host + ":587"
+	message := []byte{}
+
+	f, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read from stdin if not empty else use template for message
+	if f.Size() > 0 {
+		message, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		// Open with default editor if stdin is empty
+		tempFile := "/tmp/gomail"
+
+		// Overwrite temp file with template file
+		template, err := ioutil.ReadFile("template")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = ioutil.WriteFile(tempFile, template, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Open temp file with default editor
+		editor := os.Getenv("EDITOR")
+		cmd := exec.Command(editor, tempFile)
+		cmd.Stdin, cmd.Stdout = os.Stdin, os.Stdout
+		cmd.Run()
+
+		// Set message from temp file user just edited
+		message, err = ioutil.ReadFile(tempFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Send to self if not specify receiver
+	if len(to) <= 0 {
+		to = []string{from}
+	}
+
+	// Authenticate and send mail
+	auth := smtp.PlainAuth("", from, password, host)
+	if err := smtp.SendMail(addr, auth, from, to, message); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Mail Sent!")
+}
